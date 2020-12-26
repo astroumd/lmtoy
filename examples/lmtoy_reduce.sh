4e846f30c93aec9aad8214f3fa1f04dca539efe9
Peter Teuben <teuben@gmail.com>
2020-12-26 18:08:09 -0500

    numerous changes to deal with new style keywords, use source name as prefix, etc.etc.

--- examples/lmtoy_reduce.sh
+++ examples/lmtoy_reduce.sh
@@ -1,37 +1,56 @@
 #! /bin/bash
 #
-#  A simple OTF pipeline in bash.
+#  A simple LMT OTF pipeline in bash.
 #  Really should be written in python, but hey, here we go.
 #
-#  Note:   this will only reduce one OBSNUM.   If you place a file "$obsnum.rc"
+#  Note:   this will only reduce one OBSNUM.   If you place a file "lmtoy_$obsnum.rc"
 #          in the current directory, parameters will be read from it.
+#          If it does not exist, it will be created upon the first run
+#
+# There is no good mechanism here to make a new variable depend on re-running a certain task on which it depends
+
+version="lmtoy_reduce: 26-dec-2020"
+
+if [ -z $1 ]; then
+    echo "LMTOY>>  Usage: path=DATADIR obsnum=OBSNUM ..."
+    echo "LMTOY>>  $version"
+    exit 0
+fi
+
+
+
 
 # debug
 # set -x
 debug=0
 
 # input parameters (defaults are for the IRC benchmark)
-src=IRC
+path=IRC_data
 obsnum=79448
-
+newrc=0
+#
 makespec=1
 makecube=1
 viewspec=0
 viewcube=0
+#
 pix_list=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
 rms_cut=4
 location=0,0
 # 
 extent=300
-vlsr=-29
+#
 dv=100
 dw=250
-# these should  be computed
-#b_regions=[[-250,-150],[150,300]]
-#l_regions=[[-100,100]]
-#slice=[-350,350]
-#v_range=-100.0,100.0
-
+#
+resolution=12.5
+cell=6.25
+rmax=3
+otf_select=1
+otf_a=1.1
+otf_b=4.75
+otf_c=2
+noise_sigma=1
 
 
 #             simple keyword=value command line parser for bash - don't make any changing below
@@ -45,19 +64,18 @@ fi
 
 #             process the parameter file
 rc=lmtoy_${obsnum}.rc
-if [ -e $rc ]; then
+if [ -e $rc ] && [ $newrc = 0 ]; then
     echo "LMTOY>> reading $rc"
     source $rc
     newrc=0
+    # read cmdline again to override the rc values
+    for arg in $*; do\
+       export $arg
+    done
 else
     newrc=1
 fi
 
-#             derived parameters
-p_dir=${src}_data
-s_nc=${src}_${obsnum}.nc
-s_fits=${src}_${obsnum}.fits
-w_fits=${src}_${obsnum}.w.fits
 
 #             sanity checks
 if [ ! -d $p_dir ]; then
@@ -66,16 +84,28 @@ if [ ! -d $p_dir ]; then
 fi
 
 if [ $newrc = 1 ]; then
-    echo "# obsnum=$obsnum" > $rc
-    echo src=$src          >> $rc
-    echo vlsr=$vlsr        >> $rc    
+    echo "LMTOY>> Hang on, creating a bootstrap $rc"
+    echo "# $version"                            > $rc
+    echo "# DATE: `date +%Y-%m-%dT%H:%M:%S.%N`" >> $rc
+    echo "# obsnum=$obsnum" >> $rc
     
-    ifproc=$(ls ${p_dir}/ifproc/*${obsnum}*.nc)
-    echo "# Using $ifproc" >> $rc
+    ifproc=$(ls ${path}/ifproc/*${obsnum}*.nc)
+    if [ -z $ifproc ]; then
+	rm -f $rc
+	echo No matching obsnum=$obsnum and path=$path
+	exit 0
+    fi
+    echo "# Using ifproc=$ifproc" >> $rc
+    echo "path=$path"             >> $rc
+
+    # need a python task for this,  lmtinfo
     
-    vlsr2=$(ncdump $ifproc | grep Header.Source.Velocity | tail -1 | awk '{print $3}')
-    echo vlsr=$vlsr2 >> $rc
+    src=$(ncdump $ifproc | grep SourceName | tail -1 | awk '{print $3}'| sed 's/"//')
+    echo src=$src >> $rc
     
+    vlsr=$(ncdump $ifproc | grep Header.Source.Velocity | tail -1 | awk '{print $3}')
+    echo vlsr=$vlsr >> $rc
+
     #   w0   v0   v1     w1
     v0=$(echo $vlsr - $dv | bc -l)
     v1=$(echo $vlsr + $dv | bc -l)
@@ -86,16 +116,42 @@ if [ $newrc = 1 ]; then
     l_regions=[[$v0,$v1]]
     slice=[$w0,$w1]
     v_range=$v0,$v1
+    x_extent=$extent
+    y_extent=$extent
+
+    echo pix_list=$pix_list         >> $rc
 
     echo b_regions=$b_regions       >> $rc
     echo l_regions=$l_regions       >> $rc
-    echo slide=$slice               >> $rc
+    echo slice=$slice               >> $rc
     echo v_range=$v_range           >> $rc
+
+    echo x_extent=$x_extent         >> $rc
+    echo y_extent=$y_extent         >> $rc
+
+    # should be derived from 1.15 * c / D / skyfreq
+    echo resolution=$resolution     >> $rc
+    echo cell=$cell                 >> $rc
+
+    echo rmax=$rmax                 >> $rc
+    echo otf_a=$otf_a               >> $rc
+    echo otf_b=$otf_b               >> $rc
+    echo otf_c=$otf_c               >> $rc
+    
 
     echo "LMTOY>> this is your startup $rc file:"
     cat $rc
-    exit 0
+    echo "LMTOY>> Sleeping for 5 seconds, you can  abort, edit $rc, then continuing"
+    sleep 5
 fi
+
+
+#             derived parameters
+p_dir=${path}
+s_nc=${src}_${obsnum}.nc
+s_fits=${src}_${obsnum}.fits
+w_fits=${src}_${obsnum}.wt.fits
+
 
 
 # -----------------------------------------------------------------------------------------------------------------
@@ -153,19 +209,19 @@ if [ $makecube = 1 ]; then
 	-i $s_nc \
 	-o $s_fits \
 	-w $w_fits \
-	--resolution  11.0 \
-	--cell        5.5 \
+	--resolution  $resolution \
+	--cell        $cell \
 	--pix_list    $pix_list \
 	--rms_cut     $rms_cut \
-	--x_extent    $extent \
-	--y_extent    $extent \
-	--otf_select  1 \
-	--rmax        3 \
-	--otf_a       1.1 \
-	--otf_b       4.75 \
-	--otf_c       2 \
+	--x_extent    $x_extent \
+	--y_extent    $y_extent \
+	--otf_select  $otf_select \
+	--rmax        $rmax \
+	--otf_a       $otf_a \
+	--otf_b       $otf_b \
+	--otf_c       $otf_c \
 	--n_samples   256 \
-	--noise_sigma 1
+	--noise_sigma $noise_sigma
 fi
 
 # bug:  when rmax=5  r=12/c=2.4  malloc(): unsorted double linked list corrupted
@@ -178,7 +234,7 @@ if [ $viewcube = 1 ]; then
 		 --v_scale=1000 \
 		 --location=$location \
 		 --scale=0.000278 \
-		 --limits=-$extent,$extent,-$extent,$extent \
+		 --limits=-$x_extent,$x_extent,-$y_extent,$y_extent \
 		 --tmax_range=-1,12 \
 		 --tint_range=-1,400 \
 		 --plot_type TMAX \
@@ -197,5 +253,6 @@ if [ ! -z $ADMIT ]; then
     runa1 $s_fits
 fi
 
-echo "LMTOY>> Done with $s_fits"
+echo "LMTOY>> Created $s_fits and $w_fits"
+echo "LMTOY>> Parameter file in $rc"
 
