17926b35d24bf3a1150a4e0fbb0334fd0b72a9a8
Peter Teuben <teuben@gmail.com>
2021-01-30 11:58:16 -0500

    make rms_cut=-4 the default

--- examples/lmtoy_reduce.sh
+++ examples/lmtoy_reduce.sh
@@ -44,7 +44,7 @@ dv=100
 dw=250
 #            - parameters that directly match the SLR scripts
 pix_list=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
-rms_cut=4
+rms_cut=-4
 location=0,0
 resolution=12.5   # will be computed from skyfreq
 cell=6.25         # will be computed from resolution/2
