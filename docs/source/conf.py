0f9fa075948c118292f5af8a9bc0c1c338358ddb
Peter Teuben <teuben@gmail.com>
2021-03-09 09:40:11 -0500

    add important modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../lmtoy'))
+sys.path.insert(0, os.path.abspath('../../SpectralLineReduction/lmtslr'))
+sys.path.insert(0, os.path.abspath('../../dreampy3/dreampy3'))
 
 
 # -- Project information -----------------------------------------------------
