a257a43897fd87e7b1f4f9fbf45ed431106ac700
Peter Teuben <teuben@gmail.com>
2021-05-12 22:56:47 -0400

    read and write

--- test/sdfitsio.py
+++ test/sdfitsio.py
@@ -2,8 +2,12 @@
 #
 #  write sample SDFITS file
 #  read SDFITS file and report some properties
-#  
 #
+#  %time a=sdf.gen_data((1000,16,1,1,2048))    user 1.88 s, sys: 496 ms, total: 2.38 s
+#  %time sdf.my_write_sdfits('junk.fits',a)    user 314 ms, sys: 2.31 s, total: 2.62 s
+#
+#  (10000,16,1,1,2048)    user 20.1 s, sys: 7.09 s, total: 27.2 s      user 3.03 s, sys: 11.2 s, total: 14.2 s     1.3GB 
+
 
 import sys
 import numpy as np
@@ -44,7 +48,7 @@ class Spectra(object):
         
         
                   
-def gen_data(ndim=(256,1,1,1,256), value=None):
+def gen_data(dims=(256,1,1,1,256), value=None):
     """
     generate fake data so we can fill an SDFITS file from scratch
 
@@ -87,13 +91,13 @@ def gen_data(ndim=(256,1,1,1,256), value=None):
     d0 = np.arange(3*4).reshape(3,4)
 
     # for return
-    data = np.random.normal(-0.1,1,ndim)
+    data = np.random.normal(-0.1,1,dims)
     if True:
         data = ma.masked_invalid(data,copy=False)    
-    if True:
-        data = np.arange(data.size).reshape(ndim)
+    if False:
+        data = np.arange(data.size).reshape(dims)
 
-    print('data',data)
+    # print('data',data)
     return data
 
 def my_read(filename):
@@ -144,9 +148,75 @@ def my_read(filename):
     print("DateObs:   %s" % str(date_obs))
         
     #scan = np.unique(data2[:]['SCAN'])
+
+    # for LMT only
+    if 'DATADIMS' in header2:
+        cmd = 'dims2=%s' % header2['DATADIMS']
+        exec(cmd,globals())
+        print('DIMS2:',dims2)
+
+    spectra = data2['DATA']
+    print(spectra.shape)
+    spectra2 = spectra.reshape(dims2)
+    print(spectra2.shape)
+
+    print("Data.sum() = ", spectra.sum())
     
     hdu.close()
     
+def my_write_sdfits(filename, data):
+    # the CORE keywords/columns that need to be present
+    core = ['DATE-OBS', 'TSYS', 'DATA', 'EXPOSURE', 'TELESCOP', 'BANDWID', 'OBJECT']
+    keys = ['FEED']
+
+    dims = data.shape
+    dims1 = dims[:-1]    # all dims but nchan
+    nchan = dims[-1]     # 
+    n1 = dimsize(dims1)  # this is naxis2
+    data1 = data.reshape(n1,nchan)
+
+    print('SHAPE=',dims)
+    print('NAXIS2=',n1)
+    print('NCHAN=',nchan)
+    
+    # DATE-OBS
+    a1 = np.arange(n1, dtype=np.float64)
+    # TSYS
+    a2 = np.ones(n1, dtype=float)
+    # DATA
+    a3 = data1
+    # FEED
+    a4 = np.arange(n1, dtype=int)
+
+    print(a1.shape)
+    print(a3.shape)
+    
+    col1 = fits.Column(name='DATE-OBS', format='D',           array=a1)
+    col2 = fits.Column(name='TSYS',     format='E',           array=a2)
+    col3 = fits.Column(name='DATA',     format='%dE' % nchan, array=a3)
+    col4 = fits.Column(name='FEED',     format='I',           array=a4)
+
+    cols = fits.ColDefs([col1,col2,col3,col4])
+    hdu = fits.BinTableHDU.from_columns(cols)
+    # mark it as 'SINGLE DISH'
+    hdu.header['EXTNAME']  = 'SINGLE DISH'
+    hdu.header['EXTVER']   = 1
+    
+    # write the CORE keywords that do not vary
+    hdu.header['EXPOSURE'] = 0.1        # sec
+    hdu.header['TELESCOP'] = 'LMT/GTM'
+    hdu.header['INSTRUME'] = 'lmtoy'
+    hdu.header['OBJECT']   = 'NOISE'
+    hdu.header['BANDWID']  = 800.0e6    # Hz
+    
+    # write some provenance
+    hdu.header['ORIGIN'] = 'LMTOY test'
+    hdu.header['DATADIMS'] = str(dims)
+    
+    # finish up and write the file
+    hdu.writeto(filename, overwrite=True)
+    print("Written %s" %  filename)
+
 
 def my_write_slr(filename):
     # the CORE keywords/columns that need to be present
@@ -241,6 +311,38 @@ def my_write_rsr(filename):
     hdu.header['BANDWID']  = 800.0e6    # Hz
     hdu.writeto(filename, overwrite=True)
     print("Written %s" %  filename)
+
+def data_mask(data, mask, value=True):
+    """  Mask a ndarray by slices
+
+         data[dims]   where dims=(d1,d2,d3,....dN)
+         mask = { 0 : s0,  1:s1, .... }
+         where s0, s0 is a string representing a slice, e.g. '1:4'
+         value is normally True, but by setting it to False you can un-mask
+    """
+    print("DATA_MASK: ",mask)
+    dims = data.shape
+    ndims = len(dims)
+    cmd = 'data.mask['
+    for i in range(ndims):
+        if i in mask.keys():
+            s = mask[i]
+        else:
+            s = ':'
+        cmd = cmd + s
+        if i < ndims-1:
+            cmd = cmd + ','
+        else:
+            cmd = cmd + '] = %s' % str(value)
+    print("CMD:", cmd)
+    exec(cmd)
+    if False:
+        # this is too expensive 
+        fraction = data.count() / dimsize(dims)
+        print("Fraction of data masked: %g" % fraction)
+        
+        
+    
 
 def oper1(data,axis):
     #       data[ntime, nbeam, nband, npol, nchan]
