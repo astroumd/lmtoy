75b6851909b5072e3d158f1cd91e407f55f2c8ba
Peter Teuben <teuben@gmail.com>
2021-06-15 14:13:06 -0400

    drafting the SLpipeline, handling SEQ, RSR and 1MM

---
+++ examples/SLpipeline.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+#
+#  SLpipeline:      give an obsnum, figure out what kind of observation it is
+#                   and delegate the work to whoever it can do
+#                   $ADMIT allowed to be present.
+#
+#
+#  Note:   this will currently only reduce one OBSNUM
+#
+#  @todo   optional PI parameters
+#          htaccess
+
+version="SLpipeline: 15-jun-2021"
+
+if [ -z $1 ]; then
+    echo "LMTOY>> Usage: obsnum=OBSNUM ..."
+    exit 0
+else
+    echo "LMTOY>> $version"
+fi
+
+
+# debug
+# set -x
+debug=0
+
+# input parameters
+#            - start or restart
+path=${DATA_LMT:-data_lmt}
+obsnum=0
+
+#             simple keyword=value command line parser for bash - don't make any changing below
+for arg in $*; do\
+  export $arg
+done
+
+#             put in bash debug mode
+if [ $debug = 1 ]; then
+    set -x
+fi
+
+if [ $obsnum = 0 ]; then
+    echo No valid obsnum= given
+    exit 1
+fi
+
+rc=/tmp/lmtoy_${obsnum}.rc
+lmtinfo.py $path $obsnum > $rc
+
+source $rc
+
+if [ $instrument = "SEQ" ]; then
+    echo Processing SEQ for $ProjectId $obsnum
+    ./lmtoy_reduce.sh pdir=$ProjectId/$obsnum obsnum=$obsnum
+    # ADMIT processing done by lmtoy_reduce.sh
+elif [ $instrument = "RSR" ]; then
+    echo Processing RSR for $ProjectId $obsnum
+    
+else
+    echo Unknown instrument $instrument
+fi
