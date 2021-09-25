8a7592d88754389bcae587b82ee53993ed54591f
Peter Teuben <teuben@gmail.com>
2021-09-24 21:25:35 -0400

    add simple raw masking

--- test/Masking.py
+++ test/Masking.py
@@ -25,12 +25,15 @@
 #
 
 import sys
+import numpy as np
+import numpy.ma as ma
 
 
 class Masking(object):
     def __init__(self, filename = None, raw = False ):
         self.mask = []
         self.nifproc = 0
+        self.name = "mask"
         if filename != None:
             printf("Opening %s as masking file" % filename)
             if raw:
@@ -47,6 +50,10 @@ class Masking(object):
         for m in self.mask:
             line = line + "%s\n" % m
         return line
+
+    def setmask(self, mask):
+        print("SETMASK: found %d masked" % mask.sum())
+        self.mask = mask
 
     def print(self, header=None):
         if header != None:
@@ -83,26 +90,49 @@ class Masking(object):
         print("UNDO ",nundo)
         nmask = len(self.mask)
         if nundo > nmask:
-            print("Cannot undo %d, there are only %d" % (nundo,nmask))
+            print("Cannot undo %d, there are only %d left" % (nundo,nmask))
             return
         for i in range(nundo):
             self.mask.pop()
+        # @todo    when a legal undo was done, masking file needs to be remade
+        # @todo    when ifproc ones are undone, sdfits needs to be remade
+
+    def raw(self, flag, name=None, value=True):
+        """ allow raw python 5D flagging. The directive in the masking file is
+            #RAW   [:,1,:,:,:]
+        """
+        if flag[:4] == "#RAW":
+            p0 = flag.find("[")
+            p1 = flag.find("]")
+            # ensure p0 and p1 > 0
+            cmd = "%s%s = %s" % (self.name, flag[p0:p1+1],repr(value))
+            print("RAW flagging cmd: ", cmd)
+            exec(cmd)
+        else:
+            print("Warning: not a #RAW directive:",flag)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     a = Masking()
     print(len(a))
-    a.append('a.mask')
+    a.append("a.mask")
     print(len(a))
     
     a.print('1 -------------------------------------')
     print(len(a))
-    
-    a.append('a.mask')
+
+    a.append("a.mask")    
     a.print('2 -------------------------------------')
     print(len(a))
-    a.write('aa.mask')
+    a.write("aa.mask")
     
     a.undo(3)
     a.print('3 -------------------------------------')
     a.undo(10)
+
+
+    data = np.arange(2*2*2).reshape(2,2,1,1,2)
+    mask = np.isnan(data)
+    a.setmask(mask)
+    a.raw("#RAW  [:,1,:,:,:]")
+    print("Set mask on %d" % mask.sum())    
