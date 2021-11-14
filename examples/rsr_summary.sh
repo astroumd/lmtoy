69fa0484f8af80d1512ab9ffcf16b3a286299162
Peter Teuben <teuben@gmail.com>
2021-11-13 23:07:25 -0500

    these will eventually save system stats

---
+++ examples/rsr_summary.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#
+
+f=$1
+
+# NEMO based, 3 spectra
+
+tmp=tmp$$
+
+echo "# Min/Max/Mean/RMS of 3 spectra"
+grep 'min and max'         $f | awk -F: '{print $3}' > $tmp.1
+grep 'Mean and dispersion' $f | awk -F: '{print $3}' > $tmp.2
+paste $tmp.1 $tmp.2
---
+++ examples/seq_summary.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#
+
+f=$1
+
+# NEMO based, some cubes
+
+tmp=tmp$$
+
+
+# maps :
+#    IRC+10216_079448.ccd
+#    IRC+10216_079448.wt.ccd
+
+
+
+echo "# Min/Max/Mean/RMS of 2 cubes"
+grep 'Min and Max'  $f | awk -F: '{print $2}'   > $tmp.1
+grep 'Mean Robust'  $f | awk -F: '{print $2}'   > $tmp.2
+grep 'Sigma Robust' $f | awk -F: '{print $2}'   > $tmp.3
+paste $tmp.1 $tmp.2 $tmp.3
+
+grep QAC_STATS $f
