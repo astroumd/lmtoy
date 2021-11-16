871488068f606e4a96d29f3de04347073e922e1c
Peter Teuben <teuben@gmail.com>
2021-11-16 08:16:45 -0500

    re-arrange order

--- examples/rsr_summary.sh
+++ examples/rsr_summary.sh
@@ -7,9 +7,13 @@ f=$1
 
 tmp=tmp$$
 
-echo "# Min/Max/Mean/RMS of 3 spectra"
-grep 'min and max'         $f | awk -F: '{print $3}' > $tmp.1
-grep 'Mean and dispersion' $f | awk -F: '{print $3}' > $tmp.2
+echo "# Mean/RMS/Min/Max of 3 spectra"
+grep 'Mean and dispersion' $f | awk -F: '{print $3}' > $tmp.1
+grep 'min and max'         $f | awk -F: '{print $3}' > $tmp.2
 paste $tmp.1 $tmp.2
 
 grep QAC_STATS $f
+
+
+# cleanup
+rm $tmp.?
