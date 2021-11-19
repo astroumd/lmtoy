49ce0dbd4ac499ea5249ee9d3a713cbd57e6c5ca
Peter Teuben <teuben@gmail.com>
2021-11-19 15:10:17 -0500

    RSR pipeline/combine now using the common functions

--- examples/rsr_combine.sh
+++ examples/rsr_combine.sh
@@ -8,12 +8,12 @@
 #          2. combine the weighted maps  (comes with assumptions)     [not implemented yet]
 #             this assumes OBJECT_OBSNUM.fits and OBJECT_OBSNUM.wt.fits for all OBSNUM
 #
+#  Example:   rsr_combine.sh obsnum=33551,71610,92068 
 
-
-version="rsr_combine: 16-nov-2021"
+version="rsr_combine: 19-nov-2021"
 
 if [ -z $1 ]; then
-    echo "LMTOY>> Usage: obsnum=ON1,ON2,..."
+    echo "LMTOY>> Usage: obsnums=ON1,ON2,..."
     echo "LMTOY>> $version"
     echo ""
     echo "This will combine OBSNUM based RSR data that were reduced with rsr_reduce.sh"
@@ -25,6 +25,7 @@ else
     echo "##### Warning: this script is under development"
 fi
 
+source lmtoy_functions.sh
 
 # debug
 # set -x
@@ -32,27 +33,12 @@ debug=0
 
 # input parameters
 #            - start or restart
-obsnum=85776,85778,85824
+obsnums=0
 pdir=""
 output=""
 #            - procedural
-makecube=1
-viewcube=0
-viewnemo=1
+admit=1
 #            - parameters that directly match the SLR scripts
-unset pix_list
-rms_cut=4
-location=0,0
-resolution=12.5   # will be computed from skyfreq
-cell=6.25         # will be computed from resolution/2
-rmax=3
-otf_select=1
-otf_a=1.1
-otf_b=4.75
-otf_c=2
-noise_sigma=1
-b_order=0
-stype=2
 
 # unset a view things, since setting them will give a new meaning
 unset vlsr
@@ -76,25 +62,15 @@ else
     echo No PDIR directory used, all work in the current directory
 fi
 
-
-#             differentiate if obsnum is a file or list of obsnums
-#             set first and last obsnum, and make a list
-if [ -e $obsnum ]; then
-    # obsnum is a file
-    on0=$(grep -v ^# $obsnum | head -1)
-    on1=$(grep -v ^# $obsnum | tail -1)
-    obsnum=$(grep -v ^# $obsnum)
-else
-    # obsnum is a comma separated list
-    #         figure out the first obsnum, to inherit basic pars
-    on0=$(echo $obsnum | awk -F, '{print $1}')
-    on1=$(echo $obsnum | awk -F, '{print $NF}')
-    obsnum=$(echo $obsnum | sed 's/,/ /g')
+if [ $obsnums = 0 ]; then
+    echo obsnums= not given
+    exit 0
 fi
-obsnums=$obsnum
+
+lmtoy_decipher_obsnums
 
 rc=0
-for on in $obsnums; do
+for on in $obsnums1; do
     file=$(ls */$on/lmtoy_$on.rc)
     echo $on : $file
     if [ $rc = 0 ]; then
@@ -108,14 +84,15 @@ echo First RC will be used : $rc
 
 pdir=$ProjectId/${on0}_${on1}
 echo Using pdir=$pdir
-	  
-mkdir -p $pdir
+echo src=$src
+
+first=0
 
 # loop again to accumulate the parameter files
 rm -f $pdir/rsr.${on0}_${on1}.badlags $pdir/rsr.${on0}_${on1}.blanking  $pdir/rsr.obsnum
-for on in $obsnums; do
+for on in $obsnums1; do
     file=$(ls */$on/lmtoy_$on.rc)
-    echo ACCUM $on : $file
+    echo Using $on : $file
     cat */$on/rsr.$on.rfile    >> $pdir/rsr.${on0}_${on1}.rfile
     cat */$on/rsr.$on.badlags  >> $pdir/rsr.${on0}_${on1}.badlags
     cat */$on/rsr.$on.blanking >> $pdir/rsr.${on0}_${on1}.blanking
@@ -125,37 +102,11 @@ done
 cd $pdir
 
 blanking=rsr.${on0}_${on1}.blanking
-rfile=rsr.${on0}_${on1}.rfile
-badlags=rsr.${on0}_${on1}.badlags
+   rfile=rsr.${on0}_${on1}.rfile
+ badlags=rsr.${on0}_${on1}.badlags
 
+lmtoy_rsr1
 
-b="--badlags  $badlags"
-r="--rfile    $rfile"
-python $LMTOY/RSR_driver/rsr_driver.py rsr.obsnum  $b $r -w rsr.wf.pdf -p -b 3  > rsr1.log 2>&1
+echo OBSNUM range: $on0 .. $on1
 
-python $LMTOY/examples/rsr_sum.py -b rsr.obsnum    $b                           > rsr2.log 2>&1
-
-python $LMTOY/examples/rsr_sum.py -b $blanking  $b                              > rsr3.log 2>&1
-
-# NEMO summary spectra
-if [ ! -z $NEMO ]; then
-    echo "LMTOY>> Some NEMO post-processing"
-    dev=$(yapp_query png ps)
-    tabplot  ${src}_rsr_spectrum.txt    line=1,1 color=2 ycoord=0     yapp=${src}_rsr_spectrum.sp.$dev/$dev  debug=-1
-    tabplot  rsr.obsnum.sum.txt         line=1,1 color=2 ycoord=0     yapp=rsr.obsnum.sum.sp.$dev/$dev       debug=-1
-    tabplot  ${blanking}.sum.txt        line=1,1 color=2 ycoord=0     yapp=${blanking}.sum.sp.$dev/$dev      debug=-1
-    tabtrend ${src}_rsr_spectrum.txt 2 | tabhist - robust=t xcoord=0  yapp=${src}_rsr_spectrum.rms.$dev/$dev debug=-1
-    tabtrend rsr.obsnum.sum.txt      2 | tabhist - robust=t xcoord=0  yapp=rsr.obsnum.sum.rms.$dev/$dev      debug=-1
-    tabtrend ${blanking}.sum.txt     2 | tabhist - robust=t xcoord=0  yapp=${blanking}.sum.rms.$dev/$dev     debug=-1
-    tabstat  ${src}_rsr_spectrum.txt 2 bad=0 robust=t qac=t
-    tabstat  rsr.obsnum.sum.txt      2 bad=0 robust=t qac=t
-    tabstat  ${blanking}.sum.txt     2 bad=0 robust=t qac=t
-else
-    echo "LMTOY>> Skipping NEMO"
-fi
-
-
-echo OBSNUM range: $on0 $on1
-
-echo "LMTOY>> Parameter file used: $rc"
 
--- examples/rsr_pipeline.sh
+++ examples/rsr_pipeline.sh
@@ -13,7 +13,7 @@
 # that's perhaps for a more advanced pipeline
 #
 
-version="rsr_pipeline: 13-nov-2021"
+version="rsr_pipeline: 19-nov-2021"
 
 if [ -z $1 ]; then
     echo "LMTOY>> Usage: obsnum=OBSNUM ..."
@@ -24,6 +24,7 @@ else
     echo "LMTOY>> $version"
 fi
 
+source lmtoy_functions.sh
 
 # debug
 # set -x
@@ -32,17 +33,11 @@ debug=0
 # input parameters
 #            - start or restart
 path=${DATA_LMT:-data_lmt}
-obsnum=33551
+obsnum=0
 obsid=""
 newrc=0
 pdir=""
 #            - procedural
-makespec=1
-makecube=1
-makewf=1
-viewspec=1
-viewcube=0
-viewnemo=1
 admit=1
 
 #             simple keyword=value command line parser for bash - don't make any changing below
@@ -116,65 +111,5 @@ if [ $first == 1 ]; then
     # note $badlags is created by seed_bad_channels
 fi
 
-# first time, do a run with no badlags or rfile
-if [ $first == 1 ]; then
-    python $LMTOY/RSR_driver/rsr_driver.py rsr.obsnum  -w rsr.wf0.pdf -p -b 3   > rsr0.log 2>&1	
-fi
-    
-# output: rsr.$obsnum.badlags sbc.png
-if [ ! -e $badlags ]; then
-    python $LMTOY/examples/seek_bad_channels.py $obsnum                         > rsr4.log 2>&1
-    mv rsr.badlags $badlags
-fi
-    
-# output: $src_rsr_spectrum.txt
-b="--badlags $badlags"
-r="--rfile $rfile"
-python $LMTOY/RSR_driver/rsr_driver.py rsr.obsnum  $b $r -w rsr.wf.pdf -p -b 3  > rsr1.log 2>&1
 
-# output: rsr.obsnum.sum.txt
-python $LMTOY/examples/rsr_sum.py -b rsr.obsnum    $b                           > rsr2.log 2>&1
-
-# output: rsr.blanking.sum.txt
-python $LMTOY/examples/rsr_sum.py -b $blanking  $b                              > rsr3.log 2>&1
-
-# NEMO summary spectra
-if [ ! -z $NEMO ]; then
-    echo "LMTOY>> Some NEMO post-processing"
-    dev=$(yapp_query png ps)
-    tabplot  ${src}_rsr_spectrum.txt    line=1,1 color=2 ycoord=0     yapp=${src}_rsr_spectrum.sp.$dev/$dev  debug=-1
-    tabplot  rsr.obsnum.sum.txt         line=1,1 color=2 ycoord=0     yapp=rsr.obsnum.sum.sp.$dev/$dev       debug=-1
-    tabplot  ${blanking}.sum.txt        line=1,1 color=2 ycoord=0     yapp=${blanking}.sum.sp.$dev/$dev      debug=-1
-    tabtrend ${src}_rsr_spectrum.txt 2 | tabhist - robust=t xcoord=0  yapp=${src}_rsr_spectrum.rms.$dev/$dev debug=-1
-    tabtrend rsr.obsnum.sum.txt      2 | tabhist - robust=t xcoord=0  yapp=rsr.obsnum.sum.rms.$dev/$dev      debug=-1
-    tabtrend ${blanking}.sum.txt     2 | tabhist - robust=t xcoord=0  yapp=${blanking}.sum.rms.$dev/$dev     debug=-1
-    tabstat  ${src}_rsr_spectrum.txt 2 bad=0 robust=t qac=t
-    tabstat  rsr.obsnum.sum.txt      2 bad=0 robust=t qac=t
-    tabstat  ${blanking}.sum.txt     2 bad=0 robust=t qac=t
-else
-    echo "LMTOY>> Skipping NEMO"
-fi
-
-# -----------------------------------------------------------------------------------------------------------------
-
-# ADMIT
-if [ $admit == 1 ]; then
-    echo "LMTOY>> ADMIT post-processing" 
-    lmtoy_admit.sh ${src}_rsr_spectrum.txt
-    lmtoy_admit.sh rsr.obsnum.sum.txt
-    lmtoy_admit.sh ${blanking}.sum.txt
-else
-    echo "LMTOY>> skipping ADMIT post-processing"
-fi
-    
-# first time?
-if [ $first == 1 ]; then
-    echo "RSR: first time run, preserving a few first run figures"
-fi
-
-echo "LMTOY>> Parameter file used: $rc"
-
-rsr_readme > README.html
-
-#echo "LMTOY>> Making summary index.html:"
-#mk_index.sh
+lmtoy_rsr1
