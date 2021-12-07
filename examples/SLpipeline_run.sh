9d122033023f449abe981f78a2a52ed7fcb27e22
Peter Teuben <teuben@gmail.com>
2021-12-07 14:42:02 -0500

    renaming

--- examples/SLpipeline_run.sh
+++ examples/SLpipeline_run.sh
@@ -44,20 +44,20 @@ nobs=$(cat $run/data_lmt.log | grep -wv Cal | wc -l)
   d0=$(head -1 $run/data_lmt.log | awk '{print $1}')
   d1=$(tail -1 $run/data_lmt.log | awk '{print $1}')
     
-echo OK, $run/data_lmt.log is ready: Found $nobs obsnums from $on0 to $on1
+echo OK, $run/data_lmt.log is ready: Found $nobs non-Cal obsnums from $on0 to $on1
 echo "DATE-OBS's from run $d0 to $d1"
 
 # looping to find new obsnums (@todo skip Cal)
 while [ $sleep -ne 0 ]; do
     echo -n "checking "
-    lmtinfo.py $data | grep ^2 | grep -v failed | sort > $run/data_lmt.latest
-    on2=$(tail -1 $run/data_lmt.latest | awk '{print $2}')
+    lmtinfo.py $data | grep ^2 | grep -v failed | sort > $run/data_lmt.lag
+    on2=$(tail -1 $run/data_lmt.lag | awk '{print $2}')
     echo "$on2"
     if [ $on1 != $on2 ]; then
-	tail -1 $run/data_lmt.latest
+	tail -1 $run/data_lmt.lag
 	echo Found new obsnum=$on2
 	SLpipeline.sh obsnum=$on2 rsync=teuben@lma.astro.umd.edu:/lma1/lmt/TAP_lmt
-	cp $run/data_lmt.latest $run/data_lmt.log
+	cp $run/data_lmt.lag $run/data_lmt.log
 	on1=$on2
     fi
     sleep $sleep
