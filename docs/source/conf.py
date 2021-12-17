68bcbe81402458577737bd46ccd896124cdf4d4a
Peter Teuben <teuben@gmail.com>
2021-12-17 11:27:24 -0500

    version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ from time import localtime
 project = 'lmtoy'
 # The full version, including alpha/beta/rc tags
 # release = pdrtpy.VERSION
-release = "0.1"
+release = "0.2"
 year = str(localtime().tm_year)
 #author = pdrtpy.AUTHORS
 author = "Peter Teuben"
