35de585a2a0adabc75edbdeabd5cc02c780ed9c0
Peter Teuben <teuben@gmail.com>
2021-12-24 21:31:14 -0500

    unify output

--- examples/rsr_summary.sh
+++ examples/rsr_summary.sh
@@ -7,11 +7,11 @@ f=$1
 
 tmp=tmp$$
 
-echo "# Mean/RMS/Min/Max of spectra"
 grep 'Mean and dispersion' $f | awk -F: '{print $3}' > $tmp.1
 grep 'min and max'         $f | awk -F: '{print $3}' > $tmp.2
 paste $tmp.1 $tmp.2
 
+echo "QAC_STATS Mean/RMS/Min/Max of spectra"
 grep QAC_STATS $f
 
 # cleanup
