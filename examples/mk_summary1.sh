5d367865f4994eb61dd626bfc365e7f77f94ce94
Peter Teuben <teuben@gmail.com>
2022-01-11 21:54:41 -0500

    fix date_obs

--- examples/mk_summary1.sh
+++ examples/mk_summary1.sh
@@ -38,7 +38,7 @@ for o in ????? ?????_?????; do
     rc=$o/lmtoy_*$o.rc
     log=$o/lmtoy_*$o.log
     source $rc
-    date_obs=$(grep date-obs $rc | awk -F= '{print $2}')
+    date_obs=$(grep date_obs $rc | awk -F= '{print $2}')
     rms=$(grep QAC_STATS $log | tail -1 | awk '{print $4}')
     n=$(expr $n + 1)
 
