e06deb9ac7acc5a59fa7d38212b3d08ee2aaf8b9
Peter Teuben <teuben@gmail.com>
2022-04-06 22:05:40 -0400

    use a key

--- examples/SLpipeline_run.sh
+++ examples/SLpipeline_run.sh
@@ -12,6 +12,7 @@ rsync1=teuben@lma.astro.umd.edu:/lma1/lmt/TAP_lmt
 rsync2=lmtslr_umass_edu@unity:/nese/toltec/dataprod_lmtslr/work_lmt
 rsync=$rsync1
 dryrun=0
+key=Science
 
 
 # source lmtoy_functions.sh
@@ -44,21 +45,21 @@ if [ ! -e $run/data_lmt.log ]; then
     lmtinfo.py $data | grep ^2 | grep -v failed | sort > $run/data_lmt.log
 fi
 
-nobs=$(cat $run/data_lmt.log | grep -wv Cal | wc -l)
- on0=$(head -1 $run/data_lmt.log | awk '{print $2}')
- on1=$(tail -1 $run/data_lmt.log | awk '{print $2}')
-  d0=$(head -1 $run/data_lmt.log | awk '{print $1}')
-  d1=$(tail -1 $run/data_lmt.log | awk '{print $1}')
+nobs=$(cat $run/data_lmt.log | grep $key | wc -l)
+ on0=$(grep $key $run/data_lmt.log | head -1 | awk '{print $2}')
+ on1=$(grep $key $run/data_lmt.log | tail -1 | awk '{print $2}')
+  d0=$(grep $key $run/data_lmt.log | head -1 | awk '{print $1}')
+  d1=$(grep $key $run/data_lmt.log | tail -1 | awk '{print $1}')
     
-echo "OK, $run/data_lmt.log is ready: Found $nobs non-Cal obsnums from $on0 to $on1"
+echo "OK, $run/data_lmt.log is ready: Found $nobs $key obsnums from $on0 to $on1"
 echo "DATE-OBS's from run $d0 to $d1"
 
-# looping to find new obsnums (@todo skip Cal)
+# looping to find new Science obsnums 
 while [ $sleep -ne 0 ]; do
     ls -ltr $DATA_LMT/ifproc/ | tail -3
     echo -n "checking "
     lmtinfo.py $data | grep ^2 | grep -v failed | sort > $run/data_lmt.lag
-    on2=$(tail -1 $run/data_lmt.lag | awk '{print $2}')
+    on2=$(grep $key $run/data_lmt.lag | tail -1 | awk '{print $2}')
     echo "$on2"
     if [ $on1 != $on2 ]; then
 	tail -1 $run/data_lmt.lag
