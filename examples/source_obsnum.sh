da3f46331a5b61abd0293b0be54b2990a6b7ce91
Peter Teuben <teuben@gmail.com>
2023-02-06 14:01:03 -0500

    better report for script generator

--- examples/source_obsnum.sh
+++ examples/source_obsnum.sh
@@ -4,11 +4,17 @@
 #
 
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 [-l] ProjectId"
-    echo "   Makes a list of obsnums, sorted per source, for a given ProjectId"
+usage() {
+    echo "Usage: $0 [options] ProjectId"
+    echo "   Makes a list of obsnums, sorted per source, for a given ProjectId, ready for the script generator"
+    echo "   Options:"
     echo "   -l    report LineCheck instead of Science intent. Useful for RSR only"
     echo "   -p    report Pointing instead of Science intent."
+    echo "   -h    this help"
+}
+
+if [ -z "$1" ]; then
+    usage
     exit 0
 fi
 
@@ -23,19 +29,26 @@ if [ "$1" == "-p" ]; then
     shift
 fi
 
+if [ "$1" == "-h" ]; then
+    usage
+    exit 0
+fi
+
 pid=$1
 
-
+dat=$DATA_LMT/data_lmt.log 
 log=$WORK_LMT/tmp/$pid.obsnums.log
 g=1
 
-grep $pid $DATA_LMT/data_lmt.log | grep $intent | tabcols - 6,2,7  > $log
+grep $pid $dat | grep $intent | tabcols - 6,2,7  > $log
 
+echo "# Using $dat"
 for pid in $(tabcols $log 3 | sort | uniq); do
     echo "# $pid - $intent"
 done
 
 echo ""
+echo "on = {}"
     
 for src in $(tabcols $log 1 | sort | uniq); do
     if [ $g == 0 ]; then 
@@ -44,19 +57,24 @@ for src in $(tabcols $log 1 | sort | uniq); do
     else
 	echo ""
 	echo "on[\"$src\"] = "
+	printf " ["
 	for o in $(grep -w $src $log | tabcols - 2 | sort -n); do
 	    printf " %d," $o
 	done
+        echo "]"
     fi
 done
 
 echo ""
+echo "pars1 = {}"
 echo ""
 
 for src in $(tabcols $log 1 | sort | uniq); do
     echo "pars1[\"$src\"] = \"\""
 done
 
+echo ""
+echo "pars2 = {}"
 echo ""
 
 for src in $(tabcols $log 1 | sort | uniq); do
