5954da7dff2126b8e265c5be6b90458fe5ac1eb5
Peter Teuben <teuben@gmail.com>
2023-02-27 18:31:08 -0500

    add a red LineCheck1/2 if it exists

--- examples/rsr_summary.sh
+++ examples/rsr_summary.sh
@@ -11,8 +11,12 @@ grep 'Mean and dispersion' $f | awk -F: '{print $3}' > $tmp.1
 grep 'min and max'         $f | awk -F: '{print $3}' > $tmp.2
 paste $tmp.1 $tmp.2
 
+grep LineCheck1 $f
+grep LineCheck2 $f
+
 echo "QAC_STATS Mean/RMS/Min/Max of spectra"
 grep QAC_STATS $f
+
 
 # cleanup
 rm $tmp.?
