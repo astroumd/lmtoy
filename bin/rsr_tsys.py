c407b413bb4ab21cee8a44a802741dacf190498a
Peter Teuben <teuben@gmail.com>
2023-06-24 22:20:13 -0400

    check backend

--- bin/rsr_tsys.py
+++ bin/rsr_tsys.py
@@ -18,14 +18,18 @@ The saved plotfile has a fixed name, rsr.spectra.png (or rsr.spectra.svg)
 _version = "16-mar-2023"
 
 
+
 import sys
 import numpy as np
 from docopt import docopt
+import matplotlib
 import matplotlib.pyplot as plt
+print('mpl backend',matplotlib.get_backend())
 
 from dreampy3.redshift.utils.fileutils import make_generic_filename
 from dreampy3.redshift.netcdf import RedshiftNetCDFFile
 from dreampy3.redshift.plots import RedshiftPlot
+
 
 
 #                    command line options
