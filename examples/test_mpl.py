fba4362f5734071720ae6f7166e90d98241b2f05
Peter Teuben <teuben@gmail.com>
2023-06-26 09:58:06 -0400

    show default matplotrc file

--- examples/test_mpl.py
+++ examples/test_mpl.py
@@ -2,7 +2,9 @@
 #
 #   this reports the matplotlib backend with and without $DISPLY
 
+python -c 'import matplotlib; print(matplotlib.matplotlib_fname())'
 echo "DISPLAY: $DISPLAY"
 python -c 'import matplotlib; print(matplotlib.get_backend())'
 unset DISPLAY
 python -c 'import matplotlib; print(matplotlib.get_backend())'
+
