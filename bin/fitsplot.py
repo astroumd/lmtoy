5cc913ebbcb749b748dd64abb9245df68756d826
Peter Teuben <teuben@gmail.com>
2023-08-01 00:53:34 -0400

    less vertical while space in histogram mode

--- bin/fitsplot.py
+++ bin/fitsplot.py
@@ -76,12 +76,24 @@ bins = np.linspace(dmin, dmax, 32)
 if args.hist:
     print("BINS: ",bins)
 
-box1 = [0.1,0.1,0.8,0.8]   # full size, image
-box2 = [0.1,0.1,0.5,0.5]   # left side, image
-box3 = [0.7,0.15,0.2,0.4]  # right side, histo
+if args.hist:
+    # side by side
+    f=0.7
+    #box1 = [0.1,0.1,0.8,0.8]   # full size, image
+    box2 = [0.05,0.1,0.5/f,0.5/f]   # left side, image
+    box3 = [0.55/f,0.1,0.2,f]  # right side, histo
+else:
+    # just a square image
+    box1 = [0.1,0.1,0.8,0.8]   # full size, image
+    #box2 = [0.1,0.1,0.5,0.5]   # left side, image
+    #box3 = [0.7,0.15,0.2,0.4]  # right side, histo
 
 try:
-    fig = plt.figure(figsize=(size, size))
+    if args.hist:
+        fig = plt.figure(figsize=(size, f*size))
+    else:
+        fig = plt.figure(figsize=(size, size))
+    
     if plane < 0:
         if args.hist:
             f1 = aplpy.FITSFigure(fitsfile, figure=fig, subplot=box2)
