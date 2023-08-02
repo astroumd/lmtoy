c8f00b7d38524ca51dbcb4fd0c56fb8257143d25
Peter Teuben <teuben@gmail.com>
2023-08-01 21:25:26 -0400

    better estimate of noise

--- bin/mk_index.sh
+++ bin/mk_index.sh
@@ -240,8 +240,9 @@ for ext in "" "__0" "__1"; do
 	echo "       as function of sample time."                                 >> $html
 	echo "       Each beam should give roughly the same RMS,"                 >> $html
 	echo "       though during gridding RMS is used as a weight."             >> $html
-	echo "   Reasonable values are ~1K for the 800MHz wideband,"              >> $html
-	echo "   2K for intermediate 400MHz, and 4K for 200MHz narrowband."       >> $html
+	echo "   <br>Reasonable values are ~0.5K for the 800MHz wideband,"            >> $html
+	echo "   1K for intermediate 400MHz, and 2K for 200MHz narrowband,"       >> $html
+	echo "   for an assumed Tsys ~ 100K and 0.1s sample time."                >> $html
 	echo "           <br><IMG SRC=$base2.3.png>"                              >> $html
 	echo "         <IMG SRC=first_$base2.3.png>"                              >> $html
 	
