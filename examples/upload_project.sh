9090951362e5ca00a30bcf813f52625cb8de2116
Marc Pound <22331890+mpound@users.noreply.github.com>
2024-01-24 11:43:44 -0500

    add dryrun option.  fix checking of input dir existence

--- examples/upload_project.sh
+++ examples/upload_project.sh
@@ -6,6 +6,7 @@
 # Command line arguments, key=value.
 # Valid arguments are:
 #
+# dryrun   - If non-zero, just echo commands, don't execute them
 # envfile  - file with API credentials
 # in - input directory with project/obsnum/*.tar
 # out - output dir
@@ -19,6 +20,7 @@
 #-------------------------------------------------------------
 # Defaults:
 #-------------------------------------------------------------
+dryrun=0
 envfile=su_prod.env
 in=data_prod  # Ask Zhiyuan, this may be required to be data_prod in dvpipe
 out=ready_for_upload
@@ -35,11 +37,12 @@ done
 #-------------------------------------------------------------
 # report
 #-------------------------------------------------------------
-echo "envfile:  $envfile"
-echo "in:       $in"
-echo "out:      $out"
+echo "dryrun:    $dryrun"
+echo "envfile:   $envfile"
+echo "in:        $in"
+echo "out:       $out"
 echo "overwrite: $overwrite"
-echo "verbose:  $verbose"
+echo "verbose:   $verbose"
 
 #-------------------------------------------------------------
 # Error checking
@@ -49,7 +52,7 @@ if [ ! -f "$envfile" ]; then
     exit 255
 fi
 
-if [ ! -f "$in" ]; then
+if [ ! -d "$in" ]; then
     echo "#### Error: Input directory ${in} does not exist."
     exit 255
 fi
@@ -69,16 +72,20 @@ fi
 #-------------------------------------------------------------
 # create the YAMLs needed by dataverse in $out
 for directory in ${in}/*; do
-    if [ "$verbose" -ne 0 ]; then
+    if [ "$verbose" -ne 0 ] || [ $dryrun -ne 0 ] ; then
         echo "dvpipe -c config_prod.yaml -e ${envfile} -g lmtslr create_index -d $directory -o ${out}"
     fi
-    echo dvpipe -c config_prod.yaml -e ${envfile} -g lmtslr create_index -d $directory -o ${out}
+    if [ "$dryrun" -eq 0 ];then
+        dvpipe -c config_prod.yaml -e ${envfile} -g lmtslr create_index -d $directory -o ${out}
+    fi
 done;
 
 # upload and publish (-b major)
 for index in ${out}/*.yaml; do
-    if [ "$verbose" -ne 0 ]; then
+    if [ "$verbose" -ne 0 ] || [ $dryrun -ne 0 ] ; then
         echo "dvpipe -c config_prod.yaml -e ${envfile} -g dataset upload -a none -b major -i $index -p lmtdata"
     fi
-    echo dvpipe -c config_prod.yaml -e ${envfile} -g dataset upload -a none -b major -i $index -p lmtdata
+    if [ "$dryrun" -eq 0 ];then
+        dvpipe -c config_prod.yaml -e ${envfile} -g dataset upload -a none -b major -i $index -p lmtdata
+    fi
 done
