0495d38a5d5fee4b525b91c6e553750be16d7284
Peter Teuben <teuben@gmail.com>
2024-08-06 16:13:08 -0400

    docs

--- bin/sbatch_lmtoy2.sh
+++ bin/sbatch_lmtoy2.sh
@@ -1,11 +1,18 @@
 #! /bin/bash
+#
+# Submit a series of runfiles that depend on each other
+# If this directory is a script generator directory, also
+# "make summary"
+#
+# Usage:    sbatch_lmtoy2.sh runfile1 runfile2 ....
+#
 
-
-# sbatch_lmtoy2.sh  run1a run1b run1c run2a
+sleep=10
 
 for f in $*; do
+    # submit a file from the asrgument list, 
+    # this also stores the JOBID's in $f.jobid
     sbatch_lmtoy.sh $f
-    # this stores a list of JOB_ID's in a file $f.jid
     nj=1
     while [ $nj -gt 0 ]; do
 	nj=0
@@ -15,7 +22,7 @@ for f in $*; do
 	    if [ $? == 0 ]; then
                 echo -n "-"
 		((nj++))
-		sleep 5
+		sleep $sleep
 		continue
             else
 		echo -n "*"
@@ -25,3 +32,7 @@ for f in $*; do
     done
     echo "DONE WITH $f"
 done
+
+if [ -f Makefile ]; then
+    make summary
+fi
