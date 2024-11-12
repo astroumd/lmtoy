f2b7efa28f5641d2eb34ece82f765a7a5c84120b
Peter Teuben <teuben@gmail.com>
2024-11-12 06:59:09 +0100

    rename since lmtoy_run is used

--- bin/lmtoy_functions2.sh
+++ bin/lmtoy_functions2.sh
@@ -1,21 +1,13 @@
 # interactive helpers for those who need it
 
-lmtoy_version2="10-nov-2024"
+lmtoy_version2="12-nov-2024"
 
 echo "LMTOY>> lmtoy_functions2 $lmtoy_version2 via $0"
 echo "LMTOY>> useful aliases loaded:"
-echo "   lmtoy_run [projectID]"
-echo "   run [projectID]"
+echo "   cdrun [projectID]     --  cd to where an lmtoy_PID is"
 
-function lmtoy_run {
-    if [ -z $1 ]; then
-	(cd $WORK_LMT/lmtoy_run/; ls -d lmtoy_* | sed s/lmtoy_//g | pr -4 -t)
-    else
-	cd $WORK_LMT/lmtoy_run/lmtoy_$1
-    fi
-}
 
-function run {
+function cdrun {
     if [ -z $1 ]; then
 	(cd $WORK_LMT/lmtoy_run/; ls -d lmtoy_* | sed s/lmtoy_//g | pr -4 -t)
     else
