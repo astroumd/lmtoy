c8db06cd8e6b2b0357e1d242398d5f4f42ee05d9
Peter Teuben <teuben@gmail.com>
2025-02-16 20:59:58 -0500

    standardize how the archive is seeded

---
+++ etc/bench_archive.sh
@@ -0,0 +1,4 @@
+#   run this via bash and all three benchN.run have been run via sbatch2_lmtoy.sh
+SLpipeline.sh obsnum=33551 archive=1
+SLpipeline.sh obsnum=79448 archive=1
+SLpipeline.sh obsnum=110399 archive=1
