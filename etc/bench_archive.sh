efc086b4b4049ebaea07f55608072593ebdd6086
Peter Teuben <teuben@gmail.com>
2025-02-16 21:18:42 -0500

    typo

--- etc/bench_archive.sh
+++ etc/bench_archive.sh
@@ -1,4 +1,6 @@
-#   run this via bash and all three benchN.run have been run via sbatch2_lmtoy.sh
+#   run this via bash and all three benchN.run have been run via 
+#   sbatch_lmtoy2.sh bench1.run bench2.run bench3.run
+#
 SLpipeline.sh obsnum=33551 archive=1
 SLpipeline.sh obsnum=79448 archive=1
 SLpipeline.sh obsnum=110399 archive=1
