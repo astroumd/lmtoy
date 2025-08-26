a490c98874a3a4c517e24c8b4bc9896da3bdbc4f
Peter Teuben <teuben@gmail.com>
2025-08-26 17:31:09 -0400

    simple dump of some of the columns of the standard example_lmt.db

---
+++ bin/query_meta.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+#
+import os
+import sqlite3
+
+db_file          = 'example_lmt.db'
+table_name       = 'alma' 
+selected_columns = 'proposal_id, obsnum, processing_level, is_combined, ref_id'
+
+
+
+try:
+    database_file = os.environ['WORK_LMT'] + '/' + db_file
+    print("# ",selected_columns)
+    with sqlite3.connect(database_file) as conn:
+        cursor = conn.cursor()
+
+        # Construct the SQL query
+        sql_query = f"SELECT {selected_columns} FROM {table_name}"
+
+        # Execute the query
+        cursor.execute(sql_query)
+
+        # Fetch all rows
+        rows = cursor.fetchall()
+
+        # Print the selected columns from all rows
+        for row in rows:
+            print(*row)
+
+except sqlite3.Error as e:
+    print(f"An error occurred: {e}")
+
+
+
