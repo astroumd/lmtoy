5982c2d2db42273f7f57297d3a3ad463e347f837
Marc Pound <22331890+mpound@users.noreply.github.com>
2025-09-09 12:55:15 -0700

    add monthly option

--- bin/dataverse_stats.py
+++ bin/dataverse_stats.py
@@ -11,6 +11,7 @@ url = "https://dp.lmtgtm.org/api/info/metrics"
 parser = argparse.ArgumentParser(prog=sys.argv[0])
 parser.add_argument("--plot",    "-p", action="store_true",  help="Plot the results")
 parser.add_argument("--debug",   "-d", action="store_true",  help="debug mode")
+parser.add_argument("--monthly",   "-m", action="store_true",  help="compute monthly (differential) stats.")
 args=parser.parse_args()
 
 # cumulative monthly downloads
@@ -18,6 +19,9 @@ args=parser.parse_args()
 # number of files in collection per month
 keys = ["downloads", "datasets", "files"]
 results = dict.fromkeys(keys)
+diff = dict.fromkeys(keys)
+date = dict.fromkeys(keys)
+count = dict.fromkeys(keys)
 
 headers = {"Accept":"application/json"}
 for k in keys:
@@ -26,25 +30,35 @@ for k in keys:
         raise Exception(f"Didn't get acceptable response for {k}: {r}")
 
     results[k] = r.json()
+    #if args.monthly:
+    ary = results[k]
+    date[k] = [ary['data'][x]['date'] for x in range(len(ary['data']))]
+    count[k] = np.array([ary['data'][x]['count'] for x in range(len(ary['data']))])
+    diff[k] = count[k][1:]-count[k][:-1]
+    # first entry in diffs should be count[0]-0 = count[0]
+    diff[k] = np.insert(diff[k],0,count[k][0])
     if args.debug: 
         print(f"{results[k]=}")
+        print(f"{date[k]=}")
+        print(f"{count[k]=}")
+        print(f"{diff[k]=}")
 
-args.plot=True
 if args.plot:
     figure,ax=plt.subplots(figsize=(8,6))
     width=0.25
     multiplier=0
     xaxis = None
     for key in results:
-        ary = results[key]
-        date = [ary['data'][x]['date'] for x in range(len(ary['data']))]
         if xaxis is None:
-            xaxis=np.arange(len(date))
-        count = [ary['data'][x]['count'] for x in range(len(ary['data']))]
-        rect=ax.bar(xaxis+width*multiplier,count,width=width,label=key)
+            xaxis=np.arange(len(date[k]))
+        #rect=ax.bar(xaxis+width*multiplier,count[key],width=width,label=key)
+        ax.plot(xaxis,count[key],label=f"{key} cumulative")
+        if args.monthly:
+            rect2=ax.bar(xaxis+width*multiplier,diff[key],width=width,label=f"{key} monthly")
         multiplier=multiplier+1
         #ax.bar_label(rect,date,padding=3)
-    ax.set_xticks(xaxis+width,date)
+    ax.set_xticks(xaxis+width,date[k])
     ax.legend()
-    plt.title("Cumulative statistics for LMT dataverse")
+    plt.title("Download statistics for LMT dataverse")
+    plt.show()
 #
