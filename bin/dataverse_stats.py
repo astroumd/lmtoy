fdf1a8ef645dd40d563569f76aa1802c7f26f0af
Marc Pound <22331890+mpound@users.noreply.github.com>
2025-10-21 10:05:21 -0700

    write output files. change --file to --output (directory)

--- bin/dataverse_stats.py
+++ bin/dataverse_stats.py
@@ -6,6 +6,7 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.table import Table
+from pathlib import Path
 
 headers = { 'Accept': 'application/json'} 
 url = "https://dp.lmtgtm.org/api/info/metrics"
@@ -13,7 +14,7 @@ parser = argparse.ArgumentParser(prog=sys.argv[0])
 parser.add_argument("--plot",    "-p", action="store_true",  help="Plot the results")
 parser.add_argument("--debug",   "-d", action="store_true",  help="debug mode")
 parser.add_argument("--monthly",   "-m", action="store_true",  help="compute monthly (differential) stats.")
-parser.add_argument("--file",   "-f", action="store",  help="output file")
+parser.add_argument("--output",   "-o", action="store",  help="output directory")
 args=parser.parse_args()
 
 # cumulative monthly downloads
@@ -25,6 +26,8 @@ diff = dict.fromkeys(keys)
 date = dict.fromkeys(keys)
 count = dict.fromkeys(keys)
 
+if args.output:
+    path = Path(args.output)
 headers = {"Accept":"application/json"}
 for k in keys:
     r= requests.get(f"{url}/{k}/monthly",headers=headers)
@@ -32,7 +35,6 @@ for k in keys:
         raise Exception(f"Didn't get acceptable response for {k}: {r}")
 
     results[k] = r.json()
-    #if args.monthly:
     ary = results[k]
     date[k] = [ary['data'][x]['date'] for x in range(len(ary['data']))]
     count[k] = np.array([ary['data'][x]['count'] for x in range(len(ary['data']))])
@@ -41,20 +43,19 @@ for k in keys:
     diff[k] = np.insert(diff[k],0,count[k][0])
     if args.debug: 
         print(f"{results[k]=}")
+        print(f"{k=}")
         print(f"{date[k]=}")
         print(f"{count[k]=}")
         print(f"{diff[k]=}")
-if args.file:
-    dv = [list(date.values()) for k in date]
-    cv = [list(count.values()) for k in count]
-    fv = [list(diff.values()) for k in diff]
-    cols = []
-    for i in range(len(dv)):
-        cols.extend([dv[i],cv[i],fv[i]])
-    if args.debug:
-       print(f"{cols=}")
-
-
+    dv = date[k]
+    cv = count[k]
+    fv = diff[k]
+    if args.output:
+        t = Table(data=[dv,cv,fv],names=["date","cumulative","differential"])
+        t.meta['description']=f'Monthly dataverse download statistics for all {k}'
+        out = path / f"dataverse_{k}_stats.csv"
+        t.write(out,format="ascii.ecsv",overwrite=True)
+    
 if args.plot:
     figure,ax=plt.subplots(figsize=(8,6))
     width=0.25
@@ -72,5 +73,8 @@ if args.plot:
     ax.set_xticks(xaxis+width,date[k])
     ax.legend()
     plt.title("Download statistics for LMT dataverse")
-    plt.show()
+    if args.output:
+        plt.savefig(path/"download_stats.png")
+    else:
+        plt.show()
 #
